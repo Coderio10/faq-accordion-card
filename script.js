ef907d00876a1d6d368341d6bf665bba321f4bfc
Code Rio <96949179+Coderio10@users.noreply.github.com>
2023-03-06 13:01:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+
+// When dropdown is clicked
+// dropdown.forEach(dropdwn => {
+    // dropdown.oncllck = () => {
+        // console.log("go")
+        // h2.classList.toggle("active")
+        // dpBtn.classList.toggle("active")
+    // }
+// })
+
+// var i
+// for (i = 0; i < dropdown.length; i++) {
+    // dropdown[i].oncllck = () => {
+    //     console.log('goooo')
+    //     h2.classList.toggle("active")
+    //     dpBtn.classList.toggle("active")
+    // }
+    
+    
+// }
+    
+   
